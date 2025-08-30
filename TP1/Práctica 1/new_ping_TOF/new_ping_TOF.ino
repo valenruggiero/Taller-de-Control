e18781f2eeae5759fc172fa65d70f1bcc605d04e
Franco Berni <fberni@fi.uba.ar>
2025-08-30 17:21:22 -0300

    Edit new_ping_TOF.ino

--- TP1/Práctica 1/new_ping_TOF/new_ping_TOF.ino
+++ TP1/Práctica 1/new_ping_TOF/new_ping_TOF.ino
@@ -1,8 +1,10 @@
 #include <NewPing.h>
 
+#define MAX_DELAY_US 16000
+
 #define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     7  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 50 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define DELAY_50HZ 20000
 #define WAIT_MS    50
 
@@ -35,10 +37,15 @@ void loop() {
 
   Serial.print("La distancia es de: ");
   // Serial.print((float)uS / US_ROUNDTRIP_CM);
-  Serial.print((float)uS / US_ROUNDTRIP_CM);
-  Serial.println(" cm");
+  Serial.print((float)uS / US_ROUNDTRIP_CM*10);
+  Serial.println(" mm");
       
   unsigned long tiempoFinal = micros();                 // Marca de tiempo de finalización
   unsigned long duracion = tiempoFinal - tiempoInicio;  // Calcula la duración
-  delayMicroseconds(DELAY_50HZ-duracion);
+  unsigned long sleep = DELAY_50HZ - duracion;
+  while (sleep > MAX_DELAY_US) {
+    sleep -= MAX_DELAY_US;
+    delayMicroseconds(MAX_DELAY_US);
+  }
+  delayMicroseconds(sleep);
 }
